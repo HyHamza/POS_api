1e1f23203a7674cbf5b2535889005237c9c1c613
Hamza <talkdrove@gmail.com>
2026-08-12 12:38:53 +0500

    push

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -8,9 +8,27 @@ const { apiLimiter } = require('../middleware/rateLimit');
 router.use(apiLimiter);
 router.use(authenticateJWT);
 
-router.get('/', checkPermission('dispatcher.view'), taskController.getAllTasks);
+// GET /api/tasks - Riders can view their own tasks without dispatcher permission
+// Admins need dispatcher.view permission
+router.get('/', (req, res, next) => {
+  // Riders can always fetch their own tasks
+  if (req.user.role === 'rider') {
+    return taskController.getAllTasks(req, res, next);
+  }
+  // Admins need dispatcher.view permission
+  return checkPermission('dispatcher.view')(req, res, next);
+}, taskController.getAllTasks);
+
 router.get('/my-stats', taskController.getMyRiderStats); // Handled dynamically in controller based on rider session
-router.get('/:id', checkPermission('dispatcher.view'), taskController.getTaskById);
+
+// GET /api/tasks/:id - Similar logic for individual task
+router.get('/:id', (req, res, next) => {
+  if (req.user.role === 'rider') {
+    return taskController.getTaskById(req, res, next);
+  }
+  return checkPermission('dispatcher.view')(req, res, next);
+}, taskController.getTaskById);
+
 router.post('/', checkPermission('dispatcher.start'), taskController.createTask);
 router.put('/:id', taskController.updateTaskStatus); // Handled dynamically in controller based on task state & rider assignment
 
